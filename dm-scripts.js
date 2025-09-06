a6b9098f09f62b6294de6762a176ffe05965fe30
Averagecoder25 <90527362+Averagecoder25@users.noreply.github.com>
2025-09-05 19:13:15 -0700

    Update dm-scripts.js

--- dm-scripts.js
+++ dm-scripts.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUTT9IKCBU9iN_nxy0jbV3nVvH-qAqeIo",
+  authDomain: "dmd-store-8b03d.firebaseapp.com",
+  databaseURL: "https://dmd-store-8b03d-default-rtdb.firebaseio.com/",
+  projectId: "dmd-store-8b03d",
+  storageBucket: "dmd-store-8b03d.firebasestorage.app",
+  messagingSenderId: "711196890755",
+  appId: "1:711196890755:web:aaf9f73e3de4e284e0edb6",
+  measurementId: "G-5BBBD3H6TQ"
+};
+
+//Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 const restockDiv = document.getElementById('restockButtons');
